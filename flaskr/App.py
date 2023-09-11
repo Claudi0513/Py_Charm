fc619689590f05816bbb9515a5007a1f9270d673
Clau0513 <120037328+Clau0513@users.noreply.github.com>
2023-09-10 22:15:01 -0500

    modificaciones nuevas

--- flaskr/App.py
+++ flaskr/App.py
@@ -1,31 +1,19 @@
 from flaskr import create_app
-from .modelos import db, Cancion, Usuario, Album
+from .modelos import db, Cancion
+from flask_restful import Api
+from .vistas import vista_canciones, vista_cancion
 
-app = create_app('default')
+app = create_app("default")
 app_context = app.app_context()
 app_context.push()
 
 db.init_app(app)
 db.create_all()
 
-# prueba Cancion
-with app.app_context():
-    c = Cancion(titulo='Prueba', minutos=2, segundos=25, interprete='Juan Pablo')
-    db.session.add(c)
-    db.session.commit()
-    print(Cancion.query.all())
+#inicio la api
+api = Api(app)
 
-# prueba Usuario
-with app.app_context():
-    u = Usuario(nombre_usuario='Claudia Solano', contrasena='Abc123')
-    db.session.add(u)
-    db.session.commit()
-    print(Usuario.query.all())
+#ruta a la vista que quiero consultar parametro la clase a usar y la ruta esta clase tiene tanto get como post dentro
+api.add_resource(vista_canciones,'/canciones')
+api.add_resource(vista_cancion,'/canciones/<int:id>')
 
-
-# prueba Album
-with app.app_context():
-    a = Album(titulo_album='YHLQMDLG', anio=2020, descripcion='Es el segundo álbum del rapero y cantante Bad Bunny.', medio="CD")
-    db.session.add(a)
-    db.session.commit()
-    print(Album.query.all())
